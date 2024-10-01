31d81d8460c2f965947800389561e08de9c215c7
Thanate Dokmai <nook.microstock@gmail.com>
2024-10-01 13:24:01 +0700

    feat and fix error in obj -Nuk

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 // Exercise #1: Food Order
+const foodOder = {
+  orderName: "A0234",
+  address: "Bangkok",
+  paymentType: "Cash",
+  totalPrice: 4500,
+  restuarantName: "MK",
+};
 
-// Start coding here
+foodOder.totalPrice = 5000;
+foodOder.paymentType = "Credit Card";
+
+console.log(foodOder);
--- ex-2.js
+++ ex-2.js
@@ -6,5 +6,13 @@ const product = {
   ratings: 2.5,
   photo: null,
 };
+product.ratings = 4.5;
+product.photo =
+  "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+product.code = "IT0101010";
+product.price = 6000;
+
 let socialMediaShare = "facebookShare";
+product["facebookshare"] = 45.5;
 // Start code here
+console.log(product);
